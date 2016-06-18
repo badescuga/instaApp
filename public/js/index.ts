4fc33938e959ead02873789108fd2704d1027c32
Alex Badescu <badescuga@gmail.com>
2016-06-18 20:12:10 +0200

    work on ts compiling

---
+++ public/js/index.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../typings/index.d.ts" />
+
+class CardButton extends Component {
+    render() {
+        return (
+            <div>
+                <button>{this.props.buttonText}</button>
+            </div>
+        );
+    }
+}
+
+export default componentName;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
+/// <reference path="modules/debug/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
