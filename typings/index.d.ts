7a628f5f1810b4b91da4e45579c17dd0377ee4eb
Alex Badescu <badescuga@gmail.com>
2016-06-20 22:13:30 +0200

    small changes

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="modules/debug/index.d.ts" />
+/// <reference path="modules/object-assign/index.d.ts" />
 /// <reference path="modules/react/index.d.ts" />
